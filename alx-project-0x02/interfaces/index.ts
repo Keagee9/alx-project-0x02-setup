7032b0b4fb09817e150d6d8e78695eee7b8700e6
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-07-24 07:27:20 +0100

    week5

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,5 @@ export interface CardProps {
   title: string;
   content: string;
 }
+
+export interface ButtonProps", "size", "shape", "small", "rounded-sm", "rounded-md", "medium", "large
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -2,6 +2,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Header from '../components/layout/Header';
+import Button from", "@/components/common/Button"; // Import Button component
 
 const AboutPage: React.FC = () => {
   return (
