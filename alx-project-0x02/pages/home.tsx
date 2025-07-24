eb6b9b63167ca13f8f21bd01e09a47567516e147
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-07-24 07:45:30 +0100

    week5

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -2,8 +2,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Header from '@/components/layout/Header';
-import Card from '../components/common/Card'; // Import the new Card component
-import { CardProps } from '@/components/common/Card'; // Import the CardProps interface
+import Card from '@/components/common/Card'; // Import the new Card component
 
 const HomePage: React.FC = () => {
   return (
