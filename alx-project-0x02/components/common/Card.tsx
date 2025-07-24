db09d28021e8e58bf1ebdf89cdd1ca5aef11f756
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-07-24 07:22:44 +0100

    week5

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,7 @@
 // components/common/Card.tsx
 import React from 'react';
 import { CardProps } from '../../interfaces';
+import { type CardProps } from '../../interfaces/common/Card';
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
